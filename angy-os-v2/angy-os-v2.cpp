adba365cf9da5c0c83d840d8c79c9f9189ca19f7
Angy <angeyimportante780@gmail.com>
2025-02-14 16:18:25 +0100

    Aggiungere i file di progetto.

---
+++ angy-os-v2/angy-os-v2.cpp
@@ -0,0 +1,12 @@
+﻿// angy-os-v2.cpp: definisce il punto di ingresso dell'applicazione.
+//
+
+#include "angy-os-v2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
